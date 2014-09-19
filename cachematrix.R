f586dba35b139b8e7bb761f0544e8ee86f422530
Nathaniel Clark <natxty@gmail.com>
2014-09-18 20:45:17 -0700

    added comments and instructions

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
+## PROGRAMMING ASSIGNMENT 2
+## Lexical Scoping // Caching the Inverse of a Matrix
 
-## Write a short comment describing this function
+## USAGE EXAMPLE:
+## > source("cachematrix.R")
+## > amatrix = makeCacheMatrix(matrix(c(1,2,3,4), nrow=2, ncol=2))
+## > cacheSolve(amatrix)
+## ... will calculate, store (cache), and then return the matrix inverse
+
+## UNIT TESTS:
+## successfully solves for unit tests listed on:
+## https://class.coursera.org/rprog-007/forum/thread?thread_id=83
+
+## FUNCTIONS OVERVIEW:
+
+## makeCacheMatrix():
+## modified from the example `makeVector` code. this function
+## returns a list of functions to set/get an input matrix,
+## and set/get the computed inverse (caching by assigning with the 
+## <<- operator, which assigns the value (here, the cached inverse 
+## of the matrix) to a variable in a different environment.
 
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
@@ -16,7 +33,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve():
+## Also quite close to the example `cachemean` code, this function
+## "calculates" the inverse of the special matrix we create, initially,
+## by calling makeCacheMatrix() above.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
